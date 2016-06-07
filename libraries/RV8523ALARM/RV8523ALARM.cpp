8975104f6effd3f82ef7ce4452d60afc70ee09b2
Jens Dede <jd@comnets.uni-bremen.de>
2016-06-07 11:07:00 +0200

    Fix some whitespace errors

--- libraries/RV8523ALARM/RV8523ALARM.cpp
+++ libraries/RV8523ALARM/RV8523ALARM.cpp
@@ -44,30 +44,30 @@ void RV8523ALARM::getWeekday(uint8_t *weekday)
 
 void RV8523ALARM::resetCtrl()
 {
-	writeRegBit(0x00,7,0);
-	writeRegBit(0x00,5,0);
-	writeRegBit(0x00,4,0);
-	writeRegBit(0x00,2,0);
-	writeRegBit(0x00,1,0);
+    writeRegBit(0x00,7,0);
+    writeRegBit(0x00,5,0);
+    writeRegBit(0x00,4,0);
+    writeRegBit(0x00,2,0);
+    writeRegBit(0x00,1,0);
 
-	writeRegBit(0x01,7,0);
-	writeRegBit(0x01,6,0);
-	writeRegBit(0x01,5,0);
-	writeRegBit(0x01,4,0);
-	writeRegBit(0x01,3,0);
-	writeRegBit(0x01,2,0);
-	writeRegBit(0x01,1,0);
-	writeRegBit(0x01,0,0);
+    writeRegBit(0x01,7,0);
+    writeRegBit(0x01,6,0);
+    writeRegBit(0x01,5,0);
+    writeRegBit(0x01,4,0);
+    writeRegBit(0x01,3,0);
+    writeRegBit(0x01,2,0);
+    writeRegBit(0x01,1,0);
+    writeRegBit(0x01,0,0);
 
-	writeRegBit(0x02,3,0);
-	writeRegBit(0x02,2,0);
-	writeRegBit(0x02,1,0);
-	writeRegBit(0x02,0,0);
+    writeRegBit(0x02,3,0);
+    writeRegBit(0x02,2,0);
+    writeRegBit(0x02,1,0);
+    writeRegBit(0x02,0,0);
 
-	writeRegBit(0x0F,3,1);	//deactivate clockout
-	writeRegBit(0x0F,4,1);
-	writeRegBit(0x0F,5,1);
-	return;
+    writeRegBit(0x0F,3,1);  //deactivate clockout
+    writeRegBit(0x0F,4,1);
+    writeRegBit(0x0F,5,1);
+    return;
 }
 
 void RV8523ALARM::setAlarmTime(uint8_t min, uint8_t hour, uint8_t day, 
@@ -87,62 +87,62 @@ void RV8523ALARM::setAlarmTime(uint8_t min, uint8_t hour, uint8_t day,
 void RV8523ALARM::getAlarmTime(uint8_t *min, uint8_t *hour, uint8_t *day,
   uint8_t *weekday)
 {
-	Wire.beginTransmission(I2C_ADDR);
+    Wire.beginTransmission(I2C_ADDR);
   Wire.write(byte(0x0A));
   Wire.endTransmission();
 
-	Wire.requestFrom(I2C_ADDR, 4);
-	*min   	  = bcd2bin(Wire.read() & 0x7F);
-  *hour  	  = bcd2bin(Wire.read() & 0x3F); //24 hour mode
-  *day   	  = bcd2bin(Wire.read() & 0x3F);
+    Wire.requestFrom(I2C_ADDR, 4);
+    *min      = bcd2bin(Wire.read() & 0x7F);
+  *hour       = bcd2bin(Wire.read() & 0x3F); //24 hour mode
+  *day        = bcd2bin(Wire.read() & 0x3F);
   *weekday  = bcd2bin(Wire.read() & 0x07);
 }
 
 void RV8523ALARM::getAlarmConfig(uint8_t *min, uint8_t *hour, uint8_t *day, 
-	uint8_t *weekday, uint8_t *mAlarm, uint8_t *hAlarm, uint8_t *dAlarm,
-	uint8_t *walarm)
+    uint8_t *weekday, uint8_t *mAlarm, uint8_t *hAlarm, uint8_t *dAlarm,
+    uint8_t *walarm)
 {
-	uint8_t tmp;
+    uint8_t tmp;
   Wire.beginTransmission(I2C_ADDR);
   Wire.write(byte(0x0A));
   Wire.endTransmission();
 
   Wire.requestFrom(I2C_ADDR, 4);
-	tmp 		  = Wire.read();  
-	*min   	  = bcd2bin(tmp & 0x7F);
-	*mAlarm   = (tmp & 0x80);
-	tmp 		  = Wire.read(); 
-  *hour  	  = bcd2bin(tmp & 0x3F); //24 hour mode
-	*hAlarm 	=	(tmp & 0x80);
-	tmp 		  = Wire.read(); 
-  *day   	  = bcd2bin(tmp & 0x3F);
-	*dAlarm	  =	(tmp & 0x80);
-	tmp       = Wire.read();
-	*weekday  = bcd2bin(tmp & 0x07);
-	*walarm   = bcd2bin(tmp & 0x80);
+    tmp           = Wire.read();  
+    *min      = bcd2bin(tmp & 0x7F);
+    *mAlarm   = (tmp & 0x80);
+    tmp           = Wire.read(); 
+  *hour       = bcd2bin(tmp & 0x3F); //24 hour mode
+    *hAlarm     =   (tmp & 0x80);
+    tmp           = Wire.read(); 
+  *day        = bcd2bin(tmp & 0x3F);
+    *dAlarm   = (tmp & 0x80);
+    tmp       = Wire.read();
+    *weekday  = bcd2bin(tmp & 0x07);
+    *walarm   = bcd2bin(tmp & 0x80);
   return;
 }
 
 uint8_t RV8523ALARM::setAlarmType(char type) 
 {
   //Type: m: minute, h: hour, d: day, w: weekday
-	switch (type)
-	{
-	  case 'm':
-	    activateAlarmType(0x0A);
-	    return 1;
-	  case 'h':
-	    activateAlarmType(0x0B);
-	    return 1;
-	  case 'd':
-	    activateAlarmType(0x0C);
-	    return 1;
-	  case 'w':
-	    activateAlarmType(0x0D);
-	    return 1;
-	  default:
-	    return 0;
-	}
+    switch (type)
+    {
+      case 'm':
+        activateAlarmType(0x0A);
+        return 1;
+      case 'h':
+        activateAlarmType(0x0B);
+        return 1;
+      case 'd':
+        activateAlarmType(0x0C);
+        return 1;
+      case 'w':
+        activateAlarmType(0x0D);
+        return 1;
+      default:
+        return 0;
+    }
 }
 
 uint8_t RV8523ALARM::clearAlarmType(char type)
@@ -168,29 +168,29 @@ uint8_t RV8523ALARM::clearAlarmType(char type)
 
 void RV8523ALARM::activateAlarm()
 {
-	writeRegBit(0x00,1,1);
-	return;
+    writeRegBit(0x00,1,1);
+    return;
 }
 
 void RV8523ALARM::deactivateAlarm()
 {
-	writeRegBit(0x00,1,0);
-	return;
+    writeRegBit(0x00,1,0);
+    return;
 }
 
 void RV8523ALARM::resetInterrupt()
 {
-	writeRegBit(0x01,3,0);
-	return;
+    writeRegBit(0x01,3,0);
+    return;
 }
 
 uint8_t RV8523ALARM::readRegister(uint8_t reg)
 {
-	Wire.beginTransmission(I2C_ADDR);
-	Wire.write(byte(reg));
-	Wire.endTransmission();
-	Wire.requestFrom(I2C_ADDR,1);
-	return Wire.read();
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(byte(reg));
+    Wire.endTransmission();
+    Wire.requestFrom(I2C_ADDR,1);
+    return Wire.read();
 }
 
 //-------------------- Private --------------------
@@ -209,51 +209,51 @@ uint8_t RV8523ALARM::bcd2bin(uint8_t val)
 
 void RV8523ALARM::writeRegBit(uint8_t reg, uint8_t bitnumber, uint8_t value)
 {
-	uint8_t regval;
+    uint8_t regval;
 
-	Wire.beginTransmission(I2C_ADDR);
-	Wire.write(byte(reg));
-	Wire.endTransmission();
-	Wire.requestFrom(I2C_ADDR,1);
-	regval = Wire.read();
-	bitWrite(regval,bitnumber,value);
-	Wire.beginTransmission(I2C_ADDR);
-	Wire.write(byte(reg));
-	Wire.write(regval);
-	Wire.endTransmission();
-	return;
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(byte(reg));
+    Wire.endTransmission();
+    Wire.requestFrom(I2C_ADDR,1);
+    regval = Wire.read();
+    bitWrite(regval,bitnumber,value);
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(byte(reg));
+    Wire.write(regval);
+    Wire.endTransmission();
+    return;
 }
 
 void RV8523ALARM::activateAlarmType(uint8_t reg)
 {
-	uint8_t regval;
+    uint8_t regval;
 
-	Wire.beginTransmission(I2C_ADDR);
-	Wire.write(byte(reg));
-	Wire.endTransmission();
-	Wire.requestFrom(I2C_ADDR,1);
-	regval = Wire.read();
-	bitClear(regval,7);
-	Wire.beginTransmission(I2C_ADDR);
-	Wire.write(byte(reg));
-	Wire.write(regval);
-	Wire.endTransmission();
-	return;
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(byte(reg));
+    Wire.endTransmission();
+    Wire.requestFrom(I2C_ADDR,1);
+    regval = Wire.read();
+    bitClear(regval,7);
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(byte(reg));
+    Wire.write(regval);
+    Wire.endTransmission();
+    return;
 }
 
 void RV8523ALARM::deactivateAlarmType(uint8_t reg)
 {
-	uint8_t regval;
+    uint8_t regval;
 
-	Wire.beginTransmission(I2C_ADDR);
-	Wire.write(byte(reg));
-	Wire.endTransmission();
-	Wire.requestFrom(I2C_ADDR,1);
-	regval = Wire.read();
-	bitSet(regval,7);
-	Wire.beginTransmission(I2C_ADDR);
-	Wire.write(byte(reg));
-	Wire.write(regval);
-	Wire.endTransmission();
-	return;
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(byte(reg));
+    Wire.endTransmission();
+    Wire.requestFrom(I2C_ADDR,1);
+    regval = Wire.read();
+    bitSet(regval,7);
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(byte(reg));
+    Wire.write(regval);
+    Wire.endTransmission();
+    return;
 }
