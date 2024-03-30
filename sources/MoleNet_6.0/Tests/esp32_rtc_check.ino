6b8eaab83e3934ebf3619b78df16c8334cf740c5
karun raj koloth <46861658+Wolfestin123@users.noreply.github.com>
2024-03-30 15:41:44 +0100

    Update esp32_rtc_check.ino

--- sources/MoleNet_6.0/Tests/esp32_rtc_check.ino
+++ sources/MoleNet_6.0/Tests/esp32_rtc_check.ino
@@ -1,44 +1,15 @@
-/*
-   MIT License
-
-  Copyright (c) 2021 Felix Biego
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
-*/
-
+?* ESP32 RTC test */
 #include <ESP32Time.h>
 
-//ESP32Time rtc;
 ESP32Time rtc(3600);  // offset in seconds GMT+1
 
 void setup() {
   Serial.begin(115200);
-  rtc.setTime(30, 24, 15, 17, 1, 2021);  // 17th Jan 2021 15:24:30
-  
+  rtc.setTime(30, 24, 15, 17, 1, 2021);  // 17th Jan 2021 15:24:30  
 }
 
 void loop() {
-
-  Serial.println(rtc.getTime("%A, %B %d %Y %H:%M:%S"));   // (String) returns time with specified format 
-  
-  struct tm timeinfo = rtc.getTimeStruct();
-  //Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");   //  (tm struct) Sunday, January 17 2021 07:24:38
-  
+  Serial.println(rtc.getTime("%A, %B %d %Y %H:%M:%S"));   // (String) returns time with specified format   
+  struct tm timeinfo = rtc.getTimeStruct(); 
   delay(1000);
 }
